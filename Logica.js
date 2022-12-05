5dde261b2c3c729451b8c936649b161d3c0bf92e
Lander Sola <landersolaaguirre@gmail.com>
2022-12-05 16:18:32 +0100

    Añadiendo funcion hangman

--- Logica.js
+++ Logica.js
@@ -3,80 +3,90 @@ const buttonHint = document.getElementById("hint");
 const buttonReset = document.getElementById("reset");
 var stickman = document.getElementById("stickman");
 var context = stickman.getContext("2d");
-
-frame1()
-frame2()
-frame3()
-frame4()
-head()
-torso()
-rightArm()
-leftArm()
-rightLeg()
-leftLeg()
-
-function head() {
-  stickman = document.getElementById("stickman");
-  context = stickman.getContext("2d");
-  context.beginPath();
-  context.arc(60, 25, 10, 0, Math.PI * 2, true);
-  context.stroke();
-}
-
-function draw($pathFromx, $pathFromy, $pathTox, $pathToy) {
-  context.moveTo($pathFromx, $pathFromy);
-  context.lineTo($pathTox, $pathToy);
-  context.stroke();
-}
-
-function frame1() {
-  draw(0, 150, 150, 150);
-}
-
-function frame2() {
-  draw(10, 0, 10, 600);
-}
-
-function frame3() {
-  draw(0, 5, 70, 5);
-}
-
-function frame4() {
-  draw(60, 5, 60, 15);
-}
-
-function torso() {
-  draw(60, 36, 60, 70);
-}
-
-function rightArm() {
-  draw(60, 46, 100, 50);
-}
-
-function leftArm() {
-  draw(60, 46, 20, 50);
-}
-
-function rightLeg() {
-  draw(60, 70, 100, 100);
-}
-
-function leftLeg() {
-  draw(60, 70, 20, 100);
-}
-
-var drawArray = [
-  rightLeg,
-  leftLeg,
-  rightArm,
-  leftArm,
-  torso,
-  head,
-  frame4,
-  frame3,
-  frame2,
-  frame1
-];
+var emptyVar;
+var binary = 0;
+function frame () {
+  if(binary === 0){
+    frame1();
+    binary = 1
+  }else if (binary === 1){
+    frame2();
+    binary = 2
+  }else if (binary === 2){
+    frame3();
+    binary = 010;
+  }else if (binary === 010){
+    frame4();
+    binary = 011;
+  } else if (binary === 011){
+    head();
+    binary = 0100;
+  } else if (binary === 0100){
+    torso();
+    binary = 0101;
+  } else if (binary ===0101){
+    rightArm();
+    binary = 0111;
+  } else if (binary === 0111){
+    leftArm();
+    binary = 01010
+  } else if(binary === 01010){
+    leftLeg();
+    binary = 01011;
+  } else if (binary === 01011){
+    rightLeg();
+  }
+  }
+  
+  function head() {
+      stickman = document.getElementById("stickman");
+      context = stickman.getContext("2d");
+      context.beginPath();
+      context.arc(60, 25, 10, 0, Math.PI * 2, true);
+      context.stroke();
+    }
+    
+    function draw($pathFromx, $pathFromy, $pathTox, $pathToy) {
+      context.moveTo($pathFromx, $pathFromy);
+      context.lineTo($pathTox, $pathToy);
+      context.stroke();
+    }
+  
+    function frame1() {
+      draw(0, 150, 150, 150);
+    }
+    
+    function frame2() {
+      draw(10, 0, 10, 600);
+    }
+    
+    function frame3() {
+      draw(0, 5, 70, 5);
+    }
+    
+    function frame4() {
+      draw(60, 5, 60, 15);
+    }
+    
+    function torso() {
+      draw(60, 36, 60, 70);
+    }
+    
+    function rightArm() {
+      draw(60, 46, 100, 50);
+    }
+    
+    function leftArm() {
+      draw(60, 46, 20, 50);
+    }
+    
+    function rightLeg() {
+      draw(60, 70, 100, 100);
+    }
+    
+    function leftLeg() {
+      draw(60, 70, 20, 100);}
+  
 
 let words = ["pasas", "rutina", "espeso", "poseer", "correr", "hora", "llegar", "tienda", "vuelo", "crecer", "guerra", "brote", "marido", "lleno", "duelo", "pagas", "antena", "morena", "huella", "rutina"]
 let wordNumber
@@ -99,7 +109,7 @@ function setWord() {
     element.setAttribute("class", "letter")
   })
   
-  /**RESETEAR HANGMAN */
+  context.clearRect(0, 0, stickman.width, stickman.height)
   wordDisplay.innerHTML = " "
   wordNumber = Math.floor(Math.random() * 20)
   word = words[wordNumber].toUpperCase()
@@ -126,8 +136,10 @@ function checkLetter(i) {
     else {
       i.path[0].setAttribute("class", "incorrect")
       i.path[0].removeEventListener("click", checkLetter)
-      /**Añadir funcion para que el hangman se vaya completando */
     }
+  }
+  if(!correctLetter){
+    frame()
   }
 }
 function giveHint(){
