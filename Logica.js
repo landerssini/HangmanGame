392499d33c0d8d8beb13447dc61db134707ffa4f
Lander Sola <landersolaaguirre@gmail.com>
2022-12-05 15:19:16 +0100

    dandole logica a hint, play again y teclado, ya funciona

--- Logica.js
+++ Logica.js
@@ -16,65 +16,125 @@ rightLeg()
 leftLeg()
 
 function head() {
-    stickman = document.getElementById("stickman");
-    context = stickman.getContext("2d");
-    context.beginPath();
-    context.arc(60, 25, 10, 0, Math.PI * 2, true);
-    context.stroke();
-  }
-  
-  function draw($pathFromx, $pathFromy, $pathTox, $pathToy) {
-    context.moveTo($pathFromx, $pathFromy);
-    context.lineTo($pathTox, $pathToy);
-    context.stroke();
-  }
+  stickman = document.getElementById("stickman");
+  context = stickman.getContext("2d");
+  context.beginPath();
+  context.arc(60, 25, 10, 0, Math.PI * 2, true);
+  context.stroke();
+}
 
-  function frame1() {
-    draw(0, 150, 150, 150);
+function draw($pathFromx, $pathFromy, $pathTox, $pathToy) {
+  context.moveTo($pathFromx, $pathFromy);
+  context.lineTo($pathTox, $pathToy);
+  context.stroke();
+}
+
+function frame1() {
+  draw(0, 150, 150, 150);
+}
+
+function frame2() {
+  draw(10, 0, 10, 600);
+}
+
+function frame3() {
+  draw(0, 5, 70, 5);
+}
+
+function frame4() {
+  draw(60, 5, 60, 15);
+}
+
+function torso() {
+  draw(60, 36, 60, 70);
+}
+
+function rightArm() {
+  draw(60, 46, 100, 50);
+}
+
+function leftArm() {
+  draw(60, 46, 20, 50);
+}
+
+function rightLeg() {
+  draw(60, 70, 100, 100);
+}
+
+function leftLeg() {
+  draw(60, 70, 20, 100);
+}
+
+var drawArray = [
+  rightLeg,
+  leftLeg,
+  rightArm,
+  leftArm,
+  torso,
+  head,
+  frame4,
+  frame3,
+  frame2,
+  frame1
+];
+
+let words = ["pasas", "rutina", "espeso", "poseer", "correr", "hora", "llegar", "tienda", "vuelo", "crecer", "guerra", "brote", "marido", "lleno", "duelo", "pagas", "antena", "morena", "huella", "rutina"]
+let wordNumber
+let word
+let wordSplit
+let wordDisplay = document.querySelector("#word")
+let letter
+let hintletter
+let letterCorrectID
+let reset = document.querySelector("#reset")
+reset.addEventListener("click", setWord)
+let hint = document.querySelector("#hint")
+let letterbuttons = document.querySelectorAll(".letter")
+
+function setWord() {
+  hint.addEventListener("click", giveHint)
+  hint.setAttribute("class", "btn-2")
+  letterbuttons.forEach(element => {
+    element.addEventListener("click", checkLetter)
+    element.setAttribute("class", "letter")
+  })
+  
+  /**RESETEAR HANGMAN */
+  wordDisplay.innerHTML = " "
+  wordNumber = Math.floor(Math.random() * 20)
+  word = words[wordNumber].toUpperCase()
+  for (i = 0; i < word.length; i++) {
+    wordDisplay.innerHTML += `<div class="wordLetter">
+    <h3 class="hideLetter">${word[i]}</h3></div>`
   }
-  
-  function frame2() {
-    draw(10, 0, 10, 600);
+  hintletter = document.querySelectorAll(".hideLetter")
+}
+function checkLetter(i) {
+  let correctLetter = false
+  letter = i.path[0].textContent
+  for (a = 0; a < word.length; a++) {
+    if (word[a] == letter) {
+      letterCorrectID = document.querySelectorAll(".wordLetter")
+      letterCorrectID[a].firstElementChild.removeAttribute("class")
+      hintletter = document.querySelectorAll(".hideLetter")
+      correctLetter = true
+      console.log(i)
+    }
+    if (correctLetter) {
+      i.path[0].setAttribute("class", "correct")
+      i.path[0].removeEventListener("click", checkLetter)
+    }
+    else {
+      i.path[0].setAttribute("class", "incorrect")
+      i.path[0].removeEventListener("click", checkLetter)
+      /**Añadir funcion para que el hangman se vaya completando */
+    }
   }
-  
-  function frame3() {
-    draw(0, 5, 70, 5);
-  }
-  
-  function frame4() {
-    draw(60, 5, 60, 15);
-  }
-  
-  function torso() {
-    draw(60, 36, 60, 70);
-  }
-  
-  function rightArm() {
-    draw(60, 46, 100, 50);
-  }
-  
-  function leftArm() {
-    draw(60, 46, 20, 50);
-  }
-  
-  function rightLeg() {
-    draw(60, 70, 100, 100);
-  }
-  
-  function leftLeg() {
-    draw(60, 70, 20, 100);
-  }
-  
-  var drawArray = [
-    rightLeg,
-    leftLeg,
-    rightArm,
-    leftArm,
-    torso,
-    head,
-    frame4,
-    frame3,
-    frame2,
-    frame1
-  ];
-  
+}
+function giveHint(){
+  i= Math.floor(Math.random() * (hintletter.length))
+  hintletter[i].removeAttribute("class")
+  console.log(i)
+  hint.setAttribute("class", "used")
+  hint.removeEventListener("click", giveHint)
+}
