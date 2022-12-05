edb7a7e5cb200696deb6f2b7ca11f3170f6777bd
Marian Roibu <marianroibu32@gmail.com>
2022-12-05 15:26:56 +0100

    hangman por frames y el botton de hint randomizado

--- Logica.js
+++ Logica.js
@@ -3,17 +3,66 @@ const buttonHint = document.getElementById("hint");
 const buttonReset = document.getElementById("reset");
 var stickman = document.getElementById("stickman");
 var context = stickman.getContext("2d");
+var emptyVar;
+var binary = 0;
+var hintBinary = 0;
+const hints = ["Based in Mersyside",
+"Based in Mersyside",
+"First Welsh team to reach the Premier Leauge",
+"Owned by A russian Billionaire",
+"Once managed by Phil Brown",
+"2013 FA Cup runners up",
+"Gazza's first club"]
 
-frame1()
-frame2()
-frame3()
-frame4()
-head()
-torso()
-rightArm()
-leftArm()
-rightLeg()
-leftLeg()
+buttonHint.addEventListener("click", randomHint)
+
+function randomHint(){
+if( hintBinary === 0){
+hintShow.innerHTML = hints[Math.floor(Math.random() * hints.length)];;
+hintBinary=1;
+}else if(hintBinary === 1){
+  hintShow.innerHTML = hints[Math.floor(Math.random() * hints.length)];;
+  hintBinary=0;
+  }
+
+}
+
+buttonReset.addEventListener("click", frame)
+
+
+
+function frame () {
+if(binary === 0){
+  frame1();
+  binary = 1
+}else if (binary === 1){
+  frame2();
+  binary = 2
+}else if (binary === 2){
+  frame3();
+  binary = 010;
+}else if (binary === 010){
+  frame4();
+  binary = 011;
+} else if (binary === 011){
+  head();
+  binary = 0100;
+} else if (binary === 0100){
+  torso();
+  binary = 0101;
+} else if (binary ===0101){
+  rightArm();
+  binary = 0111;
+} else if (binary === 0111){
+  leftArm();
+  binary = 01010
+} else if(binary === 01010){
+  leftLeg();
+  binary = 01011;
+} else if (binary === 01011){
+  rightLeg();
+}
+}
 
 function head() {
     stickman = document.getElementById("stickman");
@@ -62,19 +111,4 @@ function head() {
   }
   
   function leftLeg() {
-    draw(60, 70, 20, 100);
-  }
-  
-  var drawArray = [
-    rightLeg,
-    leftLeg,
-    rightArm,
-    leftArm,
-    torso,
-    head,
-    frame4,
-    frame3,
-    frame2,
-    frame1
-  ];
-  
+    draw(60, 70, 20, 100);}
